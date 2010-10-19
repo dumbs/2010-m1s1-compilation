ae6e48edf787902380bb1d7092a6a8d1bb2d3b60
Bertrand BRUN <bertrand.brun@me.com>
2010-10-19 16:33:08 +0200

    Ajout de la fonction d'affichage de l'environnement et d'un main

---
+++ main.lisp
@@ -0,0 +1,10 @@
+(defun copytree (l)
+  (if (atom l)
+      l
+    (cons (copytree (car l))
+          (copytree (cdr l)))))
+(load "environnement")
+(load "VM/instruction")
+;; ...
+(run-test t)
+(print-env-stack exemple-env-stack)
