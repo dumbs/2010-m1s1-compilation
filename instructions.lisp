315ba199b75e5d7869e7f83d349f877085cceea0
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-11-07 05:46:34 +0100

    Oubli lors du renomage de variables.

--- instructions.lisp
+++ instructions.lisp
@@ -102,11 +102,11 @@ et termine par la liste APPEND."
            (opcode (logior opcode (position1 mode-adressage-2 table-modes-adressage))))
       (split-bytes opcode nb-opcode-bytes
                    (list (if (eq mode-adressage-1 'registre)
-							 (position1 valeur1 (get-register-list (make-vm 1)))
-						   valeur1)
+							 (position1 valeur-1 (get-register-list (make-vm 1)))
+						   valeur-1)
 						 (if (eq mode-adressage-2 'registre)
-							 (position1 valeur2 (get-register-list (make-vm 1)))
-						   valeur2))))))
+							 (position1 valeur-2 (get-register-list (make-vm 1)))
+						   valeur-2))))))
 
 ;;TODO : Faire les registres
 (defun dump-vm (vm)
